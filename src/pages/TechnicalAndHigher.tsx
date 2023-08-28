aaa1a156994ef5640bc515879dbb96f585aa8102
Elxan Civishov <elxan.civishov2000@gmail.com>
2023-08-28 10:44:17 +0400

    deleted useCallback hook

--- src/pages/TechnicalAndHigher.tsx
+++ src/pages/TechnicalAndHigher.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
